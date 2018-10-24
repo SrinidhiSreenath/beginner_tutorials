689ab992bd86c26d685a0259dfea10e521547fe3
Srinidhi Sreenath <sreenath.srinidhi@gmail.com>
2018-10-23 23:30:51 -0400

    Add doxygen style comments and format to follow Google C++ style guide

--- src/talker.cpp
+++ src/talker.cpp
@@ -1,10 +1,64 @@
+/************************************************************************************
+ * BSD 3-Clause License
+ * Copyright (c) 2018, Srinidhi Sreenath
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ ************************************************************************************/
+/**
+ *  @file    talker.cpp
+ *  @author  Srinidhi Sreenath (SrinidhiSreenath)
+ *  @date    10/23/2018
+ *  @version 1.0
+ *
+ *  @brief Source file to implement a simple ROS publisher node
+ *
+ *  @section DESCRIPTION
+ *
+ *  Source file to implement a simple ROS pubslisher node publishing a custom
+ *  message
+ *
+ */
+
+// CPP Headers
+#include <sstream>
+
+// ROS Headers
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-#include <sstream>
-
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
+ */
+/**
+ *   @brief  main function implementing the publisher node
+ *
+ *   @param  none
+ *   @return integer 0 indication successful execution
  */
 int main(int argc, char **argv) {
   /**
